b45d80e9d4f10b28fe78b55014072b710a5f6335
이소영 <sylee0314@revu.net>
2020-06-04 17:13:27 +0900

    Fix untracked files

--- app/src/main/java/net/soy/watermelon/Key.kt
+++
@@ -1,4 +0,0 @@
-package net.soy.watermelon
-
-const val KAKAO_API_KEY = "a4a7dc0b83f626751bdba54aa76936d5"
-const val HEAT_WAVE_SHELTER_SERVICE_API_KEY = "MFxvIpbalPCtOC%2BNY7RPe40eNqxb5Jt%2FhSFnQiRyBH7IN9mxtdevKpSthXvHbusKzf08XGB5sTRBQBgjpgeGFw%3D%3D"
